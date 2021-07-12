bc1c8c53e739092e0f1e816b97b097b78fda169b
Oleksandr Yepishev <Alexandrdymka@gmail.com>
2021-07-12 20:41:14 +0300

    color swither test

--- src/js/colorSwitcher.js
+++ src/js/colorSwitcher.js
@@ -1,2 +1,24 @@
 import '../sass/colorSwitcher.scss';
 
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+const body = document.querySelector('body');
+
+startBtn.addEventListener('click', colorSwitherStart);
+stopBtn.addEventListener('click', colorSwitherStop);
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function colorSwitherStart() {
+    timerId = setInterval(() => {
+    ;
+  }, 1000);
+}
+
+function colorSwitherStop() {
+    clearInterval(timerId);
+    body.style.backgroundColor = getRandomHexColor;
+};
