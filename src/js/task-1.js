7d5a25167bcc323f15a87c970149d8a850192843
Oleksandr Yepishev <Alexandrdymka@gmail.com>
2021-07-13 02:33:59 +0300

    promisification

---
+++ src/js/task-1.js
@@ -0,0 +1,27 @@
+// Подзадание 1
+
+// Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд.
+// Значением исполнившегося промиса должно быть то кол - во миллисекунд которое передали во время вызова функции delay.
+
+const delay = ms => {
+  // Change this function
+   return new Promise((resolve, reject) => {
+    
+    setTimeout(() => {
+      const isSuccess = true;
+
+      if (isSuccess) {
+        resolve(ms);
+      } else {
+        reject("error");
+      }
+    }, ms);
+  });
+};
+
+const logger = time => console.log(`Fulfilled after ${time}ms`);
+
+// Tests
+delay(2000).then(logger); // Fulfilled after 2000ms
+delay(1000).then(logger); // Fulfilled after 1000ms
+delay(1500).then(logger); // Fulfilled after 1500ms
